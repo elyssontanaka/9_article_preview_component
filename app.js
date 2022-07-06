98e9f0b6ee1b872e2caf08013ff8786918ea68b9
elyssontanaka <elysson.tanaka@gmail.com>
2022-07-06 17:30:48 -0300

    desktop UI done

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 // VARIABLES
 const share = document.querySelector(".share-container");
+const shareBtn = document.querySelector(".share-icon");
+const shareIcon = document.querySelector(".fa-share");
 
 function shareFunc() {
-    if (share.style.display === "none") {
-        share.style.display = "flex";
-    } else {
+    if (share.style.display === "flex") {
         share.style.display = "none";
+        shareBtn.style.background = "var(--light-grayish-blue)";
+        shareIcon.style.color = "var(--desaturated-dark-blue)";
+    } else {
+        share.style.display = "flex";
+        shareBtn.style.background = "var(--desaturated-dark-blue)";
+        shareIcon.style.color = "var(--light-grayish-blue)";
     }
 }
